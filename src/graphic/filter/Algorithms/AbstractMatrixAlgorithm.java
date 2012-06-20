32e16a02cd01e807d2326310992656d038c457ff
Maria Skrzypek <mmskrzyp@gmail.com>
2012-06-21 00:23:15 +0200

    Fixed Matrix Algorithm. Now it's calculating weights propertly

--- src/graphic/filter/Algorithms/AbstractMatrixAlgorithm.java
+++ src/graphic/filter/Algorithms/AbstractMatrixAlgorithm.java
@@ -1,5 +1,6 @@
 package graphic.filter.Algorithms;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 /**
@@ -9,40 +10,41 @@ import java.awt.image.BufferedImage;
  */
 public abstract class AbstractMatrixAlgorithm implements Algorithm {
 
+	BufferedImage inImage;
+
 	@Override
 	public BufferedImage execute(BufferedImage inputImage) {
-		double[][] factors = getFactors();
-		for (int i = 1; i < inputImage.getWidth() - 1; i++) {
-			for (int j = 1; j < inputImage.getHeight() - 1; j++) {
-				inputImage
-						.setRGB(i - 1,
-								j - 1,
-								(int) (inputImage.getRGB(i - 1, j - 1) * factors[0][0]));
-				inputImage.setRGB(i, j - 1,
-						(int) (inputImage.getRGB(i, j - 1) * factors[0][1]));
-				inputImage
-						.setRGB(i + 1,
-								j - 1,
-								(int) (inputImage.getRGB(i + 1, j - 1) * factors[0][2]));
-				inputImage.setRGB(i - 1, j,
-						(int) (inputImage.getRGB(i - 1, j) * factors[1][0]));
-				inputImage.setRGB(i, j,
-						(int) (inputImage.getRGB(i, j) * factors[1][1]));
-				inputImage.setRGB(i + 1, j,
-						(int) (inputImage.getRGB(i + 1, j) * factors[1][2]));
-				inputImage
-						.setRGB(i - 1,
-								j + 1,
-								(int) (inputImage.getRGB(i - 1, j + 1) * factors[2][0]));
-				inputImage.setRGB(i, j + 1,
-						(int) (inputImage.getRGB(i, j + 1) * factors[2][1]));
-				inputImage
-						.setRGB(i + 1,
-								j + 1,
-								(int) (inputImage.getRGB(i + 1, j + 1) * factors[2][2]));
+		inImage = inputImage;
+		for (int i = 1; i < inImage.getWidth() - 1; i++) {
+			for (int j = 1; j < inImage.getHeight() - 1; j++) {
+				inputImage.setRGB(i, j, getOutputRGB(i, j));
 			}
 		}
-		return inputImage;
+		return inImage;
+	}
+
+	private int getOutputRGB(int x, int y) {
+		int outputRed = 0, outputGreen = 0, outputBlue = 0;
+		Color color;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				color = new Color(inImage.getRGB(x + i - 1, y + j - 1));
+				outputRed += color.getRed() * getFactors()[i][j];
+				outputGreen += color.getGreen() * getFactors()[i][j];
+				outputBlue += color.getBlue() * getFactors()[i][j];
+			}
+		}
+		return new Color(getValidColorValue(outputRed),
+				getValidColorValue(outputGreen), getValidColorValue(outputBlue))
+				.getRGB();
+	}
+
+	private int getValidColorValue(int colorValue) {
+		if (colorValue < 0)
+			return 0;
+		else if (colorValue > 255)
+			return 255;
+		return colorValue;
 	}
 
 	public abstract double[][] getFactors();
